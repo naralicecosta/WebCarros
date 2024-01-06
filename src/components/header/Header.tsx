8a7384961ddf67d6149dfa9483ca9aeb92b549b5
naralicecosta <naralicecosta04@gmail.com>
2024-01-06 11:55:43 -0300

    feat: creating header page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,39 @@
+import logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+import {FiUser, FiLogIn} from 'react-icons/fi'
 export function Header(){
+    const signed = true
+    const loadingAuth = false
+
+
+
+
     return(
-        <div>
-            <h1>componente header</h1>
+        <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
+            <header className='flex w-full max-w-7xl items-center justify-between px-4 mx-auto'>
+                <Link to='/'>
+                    <img src={logo}
+                    alt='logo do site'/>
+                </Link>
+
+                {!loadingAuth && signed && ( //se nao estiver carregando e usuario estiver logado
+                    <Link to='/dashboard'>
+                        <div className='border-2 rounded-full p-1 border-gray-900'>
+                            <FiUser size={24} color='#000' />
+                        </div>
+                    </Link>
+                )}
+
+                {!loadingAuth && !signed &&( //se nao estiver carregando e usuario nao estiver logado
+
+                <Link to='/login'>
+                    <div className='border-2 rounded-full p-1 border-gray-900'>
+                        <FiLogIn size={24} color='#000' />
+                    </div>
+                </Link>
+                )}
+            </header>
+            
         </div>
     )
 }
