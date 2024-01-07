561f207edd3aadae50bb709820f4f5771b9e4178
naralicecosta <naralicecosta04@gmail.com>
2024-01-07 20:27:54 -0300

    feat: :sparkles: car registration page

--- src/pages/dashboard/new/New.tsx
+++ src/pages/dashboard/new/New.tsx
@@ -1,7 +1,28 @@
+import { Container } from "../../../components/container/Container"
+import { DashboardHeader } from "../../../components/panel_header/DashboardHeader"
+
+import {FiUpload} from 'react-icons/fi'
+
 export function New(){
     return(
-        <div>
-            <h1>Página de cadastrar carros</h1>
-        </div>
+        <Container>
+            <DashboardHeader />
+
+            <div className="w-full bg-white p3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+                <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+                    <div className="absolute cursor-pointer">
+                        <FiUpload size={30} color='#000'/>
+                    </div>
+                    <div className="cursor-pointer">
+                        <input type='file' accept="'image" className="opacity-0 cursor-pointer"/>
+                    </div>
+                </button>
+            </div>
+
+            <div className="w-full bg-shite p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+                <h1>teste</h1>
+
+            </div>
+        </Container>
     )
 }
