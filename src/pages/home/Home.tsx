d46a0e6bd3d8cf57fbbebc6ca9f3bb988e4889e5
naralicecosta <naralicecosta04@gmail.com>
2024-01-08 22:46:33 -0300

    feat: :sparkles: listing registered cars

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,52 @@
 import { Container } from "../../components/container/Container"
+import {useState, useEffect} from 'react'
+
+import { collection, query, getDocs, orderBy } from "firebase/firestore"
+import { db } from "../../services/firebaseConnection"
+import { Link } from "react-router-dom"
+interface CarsProps{
+    id: string;
+    name: string;
+    year: string;
+    uid: string;
+    price: number | number;
+    city: string;
+    km: string;
+    images: CarImageProps[]
+}
+interface CarImageProps{
+    name: string;
+    uid: string;
+    url: string
+}
 
 export function Home(){
+    const [cars, setCars] = useState<CarsProps[]>([])
+
+    useEffect(() => {
+        function loadCar(){
+            const carsRef = collection(db, "cars");
+            const queryRef = query(carsRef, orderBy("created", "desc"))
+
+            getDocs(queryRef)
+            .then((snapshot) => {
+                let listCars = [] as CarsProps[]
+                snapshot.forEach(doc => {
+                    listCars.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        year: doc.data().year,
+                        km: doc.data().km,
+                        city: doc.data().city,
+                        price: doc.data().price,
+                        images: doc.data().images,
+                        uid: doc.data().uid,
+                    })
+                })
+                setCars(listCars)
+            })
+        }
+    })
     return(
         <Container>
         <section className=" bg-white p-4 rounded-lg w-full max-x-3xl mx-auto flex justify-center items-center gap-2">
@@ -19,26 +65,31 @@ export function Home(){
         </h1>
         <main className="grid gird-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
+            {cars.map(car => (
+                 <Link key={car.id} to={`car/${car.id}`}>
+                    <section className="w-full bg-white rounded-lg">
+                        <img
+                        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                        src={car.images[0].url}
+                        alt="carro"/>
+                        <p className="font-bols mt-1 mb-2 px-2">{car.name}</p>
+        
+                        <div className="flex flex-col px-2">
+                            <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km}</span>
+                            <strong className="text-black font-medium text-xl">{car.price}</strong>
+                        </div>
+        
+                        <div className="w-full h-px bg-slate-200 my-2">
 
-            <section className="w-full bg-white rounded-lg">
-                <img
-                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                 src='https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231109/volkswagen-jetta-2.0-350-tsi-gasolina-gli-dsg-wmimagem11182289826.jpg?s=fill&w=552&h=414&q=60'
-                alt="carro"/>
-                <p className="font-bols mt-1 mb-2 px-2">JETTA 2.0 350 TSI </p>
-
-                <div className="flex flex-col px-2">
-                    <span className="text-zinc-700 mb-6">Ano 2023 |0KM</span>
-                    <strong className="text-black font-medium text-xl">R$ 222.000</strong>
-                </div>
-
-                <div className="w-full h-px bg-slate-200 my-2"></div>
-                <div className="px-2 pb-2">
-                    <span className="text-zinc-700">
-                        Natal - RN
-                    </span>
-                </div>
-            </section>
+                        </div>
+                        <div className="px-2 pb-2">
+                            <span className="text-zinc-700">
+                                {car.city}
+                            </span>
+                        </div>
+                    </section>
+                 </Link>
+            ))}
 
             
 
