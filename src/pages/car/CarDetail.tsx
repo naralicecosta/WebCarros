0f6902e855e66ce21b048f32bbdae5c431b7a759
naralicecosta <naralicecosta04@gmail.com>
2024-01-15 19:19:41 -0300

    feat: :sparkles: finalizing details

--- src/pages/car/CarDetail.tsx
+++ src/pages/car/CarDetail.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react"
 import { Container } from "../../components/container/Container"
 import {FaWhatsapp} from   'react-icons/fa'
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { getDoc, doc } from "firebase/firestore"
 import { db } from "../../services/firebaseConnection"
-import { Swiper } from "swiper/types"
-import { SwiperSlide } from "swiper/element"
+import { Swiper , SwiperSlide } from "swiper/react"
 
 interface CarProps{
     id: string,
@@ -32,6 +31,7 @@ export function CarDetail(){
     const {id} = useParams()
     const [car, setCar] = useState<CarProps>()
     const [sliderPerview, setSliderPerview] = useState<number>(2)
+    const navigate = useNavigate()
 
     useEffect(()=>{
         async function loadCar() {
@@ -40,6 +40,10 @@ export function CarDetail(){
             const docRef = doc(db, "cars", id)
             getDoc(docRef)
             .then((snapshot) => {
+
+                if (!snapshot.data()){
+                    navigate("/")
+                }
                 setCar({
                     id: snapshot.id,
                     name: snapshot.data()?.name,
@@ -74,72 +78,76 @@ export function CarDetail(){
 
         return() =>{
             window.removeEventListener("resize", handleResize)
+
         }
     },[])
 
-    return(
-        <>
-        
+    return (
         <Container>
-            <Swiper
-            slidesPerview={sliderPerview}
-            pagination={{clickable: true}}
-            navigation>
-                {car?.images.map(image => (
-                    <SwiperSlide key={image.name}>
-                        <img 
-                        src=[image.url}
-                        className="w-full h-96 object-cover"/>
-                    </SwiperSlide>
-                ))}
-
-            </Swiper>
-            {car && (
-                <main className="w-full bg-white rounded-lg p-6 my-4">
-                    <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
-                        <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
-                        <h1  className="font-bold text-3xl text-black">R${car?.price}</h1>
-                    </div>
-                    <p>{car?.model}</p>
-
-                    <div className="flex w-full gap-6 my-4">
-                        <div className="flex flex-col gap-4">
-                            <div>
-                                <p>Cidade</p>
-                                <strong>{car?.city}</strong>
-                            </div>
-
-                            <div>
-                                <p>Ano</p>
-                                <strong>{car?.year}</strong>
-                            </div>
-                        </div>
-
-
-                        <div className="flex flex-col gap-4">
-                            <div>
-                                <p>KM</p>
-                                <strong>{car?.km}</strong>
-                            </div>
-
-                        </div>
-                    </div>
-                    <strong>Descrição:</strong>
-                    <p className="mb-4">{car?.description}</p>
-
-                    <strong>Telefone / Whatsapp</strong>
-                    <p>{car?.whatsapp}</p>
-
-                    <p className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium">
-                        Conversar com vendedor
-                        <FaWhatsapp size={26} color='#fff"/>
-                    </p>
-
-
-                </main>
           
-            )}
-        </Container> 
-    </>
-    )
-}
+          {car && (
+            <Swiper
+            slidesPerView={sliderPerview}
+            pagination={{ clickable: true }}
+            navigation
+          >
+            {car?.images.map( image => (
+              <SwiperSlide key={image.name}>
+                <img
+                  src={image.url}
+                  className="w-full h-96 object-cover"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          )}
+    
+          { car && (
+          <main className="w-full bg-white rounded-lg p-6 my-4">
+            <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+              <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+              <h1 className="font-bold text-3xl text-black">R$ {car?.price}</h1>
+            </div>
+            <p>{car?.model}</p>
+            
+            <div className="flex w-full gap-6 my-4">
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>Cidade</p>
+                  <strong>{car?.city}</strong>
+                </div> 
+                <div>
+                  <p>Ano</p>
+                  <strong>{car?.year}</strong>
+                </div> 
+              </div>
+    
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>KM</p>
+                  <strong>{car?.km}</strong>
+                </div> 
+              </div>
+            </div>
+    
+            <strong>Descrição:</strong>
+            <p className="mb-4">{car?.description}</p>
+            
+    
+            <strong>Telefone / WhatsApp</strong>
+            <p>{car?.whatsapp}</p>
+    
+            <a
+                href={`https://api.whatsapp.coom/send?phone=${car?.whatsapp}&text=Olá vi esse ${car?.name}no site WebCarros e fiquei interessado`}
+                target="_blank"
+                className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium"
+            >
+              Conversar com vendedor
+              <FaWhatsapp size={26} color="#FFF" />
+            </a>
+    
+          </main>
+          )}
+        </Container>
+      )
+    }
